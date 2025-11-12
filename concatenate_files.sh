0139860a6bbe52bed38d032d156bbfecb2003b3f
delcroip <patrick.delcroix@swisstph.ch>
2025-11-12 12:55:25 +0100

    fix move await demo in int script

--- concatenate_files.sh
+++ concatenate_files.sh
@@ -2,5 +2,5 @@
 
 mkdir output
 cd database\ scripts
-cat 00_dump.sql 0[2345]_*.sql demo_db.sql > ../output/fullDemoDatabase.sql
+cat 00_dump.sql 0[2345]_*.sql ../scripts/demo_db.sql > ../output/fullDemoDatabase.sql
 cat 00_dump.sql 0[2345]_*.sql > ../output/EmptyDatabase.sql
